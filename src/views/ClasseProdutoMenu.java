55f1fb749a499cd734c9c60581272cc5dd780ca1
Antônio Ximenes <almeid.ximenes@gmail.com>
2024-10-10 20:52:47 -0300

    parte 2

---
+++ src/views/ClasseProdutoMenu.java
@@ -0,0 +1,47 @@
+package views;
+
+public class ClasseProdutoMenu extends BaseMenu {
+    
+    public ClasseProdutoMenu(){
+        super();
+    }
+
+    @Override
+    public void ExibirMenu() {
+        int opcao = 0;
+
+        while(opcao != 9) {
+           System.out.println();   
+        }
+    }
+
+    @Override
+    public void Listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Listar'");
+    }
+
+    @Override
+    public void Localizar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Localizar'");
+    }
+
+    @Override
+    public void Adicionar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Adicionar'");
+    }
+
+    @Override
+    public void Atualizar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Atualizar'");
+    }
+
+    @Override
+    public void Remover() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Remover'");
+    }
+}
